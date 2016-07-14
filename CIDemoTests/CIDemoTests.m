b1a83d309270fad1741ac2d03fb78b3d22dcd581
Kartik Sharma <kartiksharma@innovationm-26.local>
2016-07-14 17:16:19 +0530

    Testing Server again

--- CIDemoTests/CIDemoTests.m
+++ CIDemoTests/CIDemoTests.m
@@ -28,7 +28,7 @@
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     
-    // Testing Server new
+    // Testing Server new again
 }
 
 - (void)testPerformanceExample {
